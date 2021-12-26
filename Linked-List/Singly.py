619be450f6bf3e9e1a7088272d62cb21a1b2dc6f
milahnmartin <66036885+milahnmartin@users.noreply.github.com>
2021-12-26 21:35:28 +0200

    Added Singly Linked List
    Added Linked List folder, added sinly linked list

---
+++ Linked-List/Singly.py
@@ -0,0 +1,65 @@
+class Node:
+    def __init__(self,val) -> None:
+        self.val = val
+        self.next = None
+
+
+class sllist:
+    def __init__(self) -> None:
+        self.head = None
+    
+    def add_head(self,val) -> None:
+        if self.head is None:
+            self.head = Node(val)
+            return
+        
+        new_node = Node(val)
+
+        new_node.next = self.head
+        self.head = new_node
+    
+    def add_tail(self,val):
+        if self.head is None:
+            self.head = Node(val)
+            return
+        
+        new_node = Node(val)
+        current_node = self.head
+
+        while current_node.next:
+            current_node = current_node.next
+
+
+        current_node.next = new_node
+    
+
+    def list_size(self) -> int:
+        list_counter = 0
+        current_node = self.head
+
+        if self.head is None:
+            return 0
+        elif self.head.next is None:
+            return 1
+
+        
+        while current_node:
+            list_counter +=1
+        
+        return list_counter
+    
+
+    def iterate_list(self) -> list:
+        vals = []
+
+        if self.head is None:
+            return []
+        else:
+            current_node = self.head
+            while current_node:
+                vals.append(current_node.val)
+                current_node = current_node.next
+            
+            return vals
+    
+    
