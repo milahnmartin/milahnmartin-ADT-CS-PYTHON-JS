6a9dd0f2002e1770b6bded1bfd8e827552ab2c37
milahnmartin <66036885+milahnmartin@users.noreply.github.com>
2021-12-28 21:13:57 +0200

    added iterate to doubly

--- Linked-List/Doubly.py
+++ Linked-List/Doubly.py
@@ -32,4 +32,23 @@ class dllist:
         
         curr_node.next = new_node
         new_node.prev = curr_node
+    
+    def iterate(self):
+        if self.head is None:
+            return None
         
+        curr_node = self.head
+        while curr_node:
+            print(curr_node.val)
+            curr_node = curr_node.next
+
+
+
+
+p = dllist()
+
+
+for i in range(5,1000,3):
+    p.add_tail(i)
+
+p.iterate()
