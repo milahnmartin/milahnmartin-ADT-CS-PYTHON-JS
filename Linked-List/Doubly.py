dd12fd0ef310a64e56924c18bf7b384014e91e85
milahnmartin <66036885+milahnmartin@users.noreply.github.com>
2022-01-03 18:33:31 +0200

    Added Reverse Method for Doubly and Perfect Print
    Added methods to Doubly that reverses the list and also perfect prints it so that you can see the prev curr and next nodes

--- Linked-List/Doubly.py
+++ Linked-List/Doubly.py
@@ -41,6 +41,55 @@ class dllist:
         while curr_node:
             print(curr_node.val)
             curr_node = curr_node.next
+    
+    def reverse_list(self) -> None:
+        if self.head is None:
+            return
+        
+        prev_pointer = None
+        next_pointer = None
+        current_pointer = self.head
+
+        while current_pointer:
+            next_pointer = current_pointer.next
+            current_pointer.next = prev_pointer
+            prev_pointer = current_pointer
+            current_pointer.prev = next_pointer
+            current_pointer = next_pointer
+        
+        self.head = prev_pointer
+    
+
+    def perfect_print(self) -> None:
+        if self.head is None:
+            return
+        
+        curr_node = []
+        prev_node = []
+        next_node = []
+
+        curr = self.head
+
+        while curr:
+            if curr.prev is None:
+                prev_node.append(None)
+                curr_node.append(curr.val)
+                next_node.append(curr.next.val)
+            elif curr.next is None:
+                prev_node.append(curr.prev.val)
+                curr_node.append(curr.val)
+                next_node.append(None)
+            else:
+                prev_node.append(curr.prev.val)
+                curr_node.append(curr.val)
+                next_node.append(curr.next.val)
+            
+            curr = curr.next
+
+        
+
+        for p,c,n in zip(prev_node,curr_node,next_node):
+            print(f'Prev-Node:{p}, Curr-Node:{c}, Next-Node:{n}')
 
 
 
@@ -48,7 +97,10 @@ class dllist:
 p = dllist()
 
 
-for i in range(5,1000,3):
+for i in range(5,500,3):
     p.add_tail(i)
 
-p.iterate()
+p.perfect_print()
+print(f'[REVERSED DOUBLY LINKED LIST]')
+p.reverse_list()
+p.perfect_print()
